94e87444dbaccbfaab103b6ac1a9fde9ae5042b4
NicolePotter <ncruz071@fiu.edu>
2024-07-05 19:30:02 -0400

    Initial Commit

---
+++ NiceBabies/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NiceBabies
+//
+//  Created by Nicole Potter on 7/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NiceBabies/NiceBabiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiceBabiesApp.swift
+//  NiceBabies
+//
+//  Created by Nicole Potter on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct NiceBabiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
