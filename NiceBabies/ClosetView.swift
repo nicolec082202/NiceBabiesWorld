05bdab549cff0127350bb02113a57d50e67fe2f0
Carolin <carolinhromero@gmail.com>
2024-11-04 19:54:25 -0500

    commiting closetview UI

--- NiceBabies/ClosetView.swift
+++ NiceBabies/ClosetView.swift
@@ -1,64 +1,228 @@
-import SwiftUI  
 
-// Define the ClosetView structure conforming to the View protocol
+import SwiftUI
+
+struct CarouselItemModel: Identifiable {
+    let id: Int
+    let name: String
+    let image: Image
+}
+
+extension Color {
+    // RGB Values
+    static let dropshot = Color(red: 0.976470588, green: 0.5333333, blue: 0.0196078431)
+    static let doublebounce = Color(red: 0.952941176, green: 0.337254902, blue: 0.584313725)
+}
+
 struct ClosetView: View {
+    @StateObject var UIState = UIStateModel()
+    @Binding var equippedBaby: String
+    
     var body: some View {
-        // Use a VStack to vertically stack elements
-        VStack {
+        let spacing: CGFloat = 130
+        let cardHeight: CGFloat = 180
+        
+        let items = [
+            CarouselItemModel(id: 0, name: "Fish", image: Image("NiceBaby_Fish")),
+            CarouselItemModel(id: 1, name: "Monkey", image: Image("NiceBaby_Monkey")),
+            CarouselItemModel(id: 2, name: "Sheep", image: Image("NiceBaby_Sheep")),
+            CarouselItemModel(id: 3, name: "Panda", image: Image("NiceBaby_Panda")),
+            CarouselItemModel(id: 4, name: "Cow", image: Image("NiceBaby_Cow")),
+            CarouselItemModel(id: 5, name: "Rabbit", image: Image("NiceBaby_Rabbit")),
             
-            // HStack to display the "Collection" label and a plus icon horizontally
-            HStack {
-                Text("Collection")  // Display the text "Collection"
-                Image(systemName: "plus.circle")  // Display a plus circle icon
+        ]
+        
+        ZStack {
+            Canvas{
+                Carousel(
+                    numberOfItems: CGFloat(items.count),
+                    spacing: spacing
+                ) {
+                    ForEach(items) { item in
+                        Item(
+                            _id: Int(item.id),
+                            spacing: spacing,
+                            cardHeight: cardHeight
+                        ) {
+                            VStack {
+                                item.image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(maxWidth: .infinity, minHeight: 540)
+                            }
+                            .cornerRadius(8)
+                            .transition(AnyTransition.slide)
+                            .animation(.spring)
+                        }
+                    }
+                }
+                //This dynamically updates the baby that is currently in view (activecard)
+                .environmentObject(UIState)
             }
-            .font(.system(size: 45))  // Set the font size for both elements
-            .position(x: 200, y: 70)  // Position the HStack within the view
-            
-            // HStack to display the first row of baby images
-            HStack {
-                Image("NiceBaby_Fish")  // Load the fish-themed baby image
-                    .resizable()  // Allow the image to resize
-                    .aspectRatio(contentMode: .fit)  // Maintain aspect ratio
+            // Page control indicator
+            HStack(spacing: 8) {
+                ForEach(0..<items.count, id: \.self) { index in
+                    Circle()
+                        .fill(UIState.activeCard == index ? Color.doublebounce : Color.dropshot)
+                        .frame(width: 10, height: 10)
+                        .animation(.easeInOut, value: UIState.activeCard)
+                }
+            }
+            .padding(.top, 485)
+            VStack {
+                Button(action: {
+                    let selectedBaby = items[UIState.activeCard].name
+                    print("\(selectedBaby)") //debugging purposes
+                    equippedBaby = "NiceBaby_\(selectedBaby)"
+                    //print("\(equippedBaby)") //debugging purposes
+                }) {
+                    Text("EQUIP")
+                        .font(.system(size: 20, weight: .regular))
+                        .foregroundColor(.white)
+                        .frame(width: 120, height: 50)
+                        .background(Color.dropshot)
+                        .cornerRadius(25)
+                }
+                .padding(.top, 590)
                 
-                Image("NiceBaby_Monkey")  // Load the monkey-themed baby image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                
-                Image("NiceBaby_Sheep")  // Load the sheep-themed baby image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
             }
             
-            // Add a horizontal divider between rows
-            Divider()
-                .frame(width: 320, height: 10)  // Set the divider’s size
-                .overlay(.black)  // Set the divider’s color to black
-            
-            // HStack to display the second row of baby images
-            HStack {
-                Image("NiceBaby_Panda")  // Load the panda-themed baby image
-                    .resizable()
+        }
+    }
+    
+    public class UIStateModel: ObservableObject {
+        @Published var activeCard: Int = 0
+        @Published var screenDrag: Float = 145
+    }
+    
+    struct Carousel<Items : View> : View {
+        // Initializing necessary properties for the carousel
+        // Including a timer for automatic sliding
+        let items: Items
+        let numberOfItems: CGFloat
+        let spacing: CGFloat
+        let totalSpacing: CGFloat
+        let cardWidth: CGFloat
+        
+        // Gesture state for drag detection
+        @GestureState var isDetectingLongPress = false
+        
+        // Accessing UI state
+        @EnvironmentObject var UIState: UIStateModel
+        
+        @inlinable public init(
+            numberOfItems: CGFloat,
+            spacing: CGFloat,
+            @ViewBuilder _ items: () -> Items) {
                 
-                Image("NiceBaby_Rabbit")  // Load the rabbit-themed baby image
-                    .resizable()
+                // Setting up carousel parameters
+                self.items = items()
+                self.numberOfItems = numberOfItems
+                self.spacing = spacing
+                self.totalSpacing = (numberOfItems - 1) * spacing
+                self.cardWidth = (UIScreen.main.bounds.width - (spacing * 3)) / 2
                 
-                Image("NiceBaby_Cow")  // Load the cow-themed baby image
-                    .resizable()
             }
+        
+        // Body of the carousel view
+        var body: some View {
+            let screenWidth = UIScreen.main.bounds.width
             
-            // Add another horizontal divider between rows
-            Divider()
-                .frame(width: 320, height: 10)
-                .overlay(.black)
+            // Calculate the position that centers the active card
+            let centralizeOffset = (screenWidth - cardWidth) / 2.0
             
-            Spacer()  // Add a spacer to push content to the top
+            // Calculate the offset for the current active card
+            let activeOffset = CGFloat(UIState.activeCard) * -(cardWidth + spacing)
+            
+            // Combine offsets with drag
+            let totalOffset = centralizeOffset + activeOffset + CGFloat(UIState.screenDrag)
+            
+            return HStack(alignment: .top, spacing: spacing) {
+                items
+            }
+            .offset(x: totalOffset, y: 0)
+            .animation(.spring(response: 0.5, dampingFraction: 0.8), value: UIState.activeCard)
+            .gesture(
+                DragGesture()
+                    .updating($isDetectingLongPress) { currentState, gestureState, transaction in
+                        UIState.screenDrag = Float(currentState.translation.width) + 145
+                    }
+                    .onEnded { value in
+                        UIState.screenDrag = 145
+                        // Move right
+                        if (value.translation.width < -50) && UIState.activeCard < Int(numberOfItems) - 1 {
+                            withAnimation(.spring()) {
+                                UIState.activeCard = UIState.activeCard + 1
+                            }
+                            let impactMed = UIImpactFeedbackGenerator(style: .medium)
+                            impactMed.impactOccurred()
+                        }
+                        // Move left
+                        if (value.translation.width > 50) && UIState.activeCard > 0 {
+                            withAnimation(.spring()) {
+                                UIState.activeCard = UIState.activeCard - 1
+                            }
+                            let impactMed = UIImpactFeedbackGenerator(style: .medium)
+                            impactMed.impactOccurred()
+                        }
+                    }
+            )
+        }
+    }
+    struct Canvas<Content : View> : View {
+        let content: Content
+        @EnvironmentObject var UIState: UIStateModel
+        
+        @inlinable init(@ViewBuilder _ content: () -> Content) {
+            self.content = content()
+            
+        }
+        
+        //Background image
+        var body: some View {
+            content
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                .background(Image("closet_view")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .blur(radius: 5, opaque: true))
+        }
+    }
+    
+    struct Item<Content: View>: View {
+        @EnvironmentObject var UIState: UIStateModel
+        let cardWidth: CGFloat
+        let cardHeight: CGFloat
+        var _id: Int
+        var content: Content
+        
+        @inlinable public init(
+            _id: Int,
+            spacing: CGFloat,
+            //widthOfHiddenCards: CGFloat,
+            cardHeight: CGFloat,
+            @ViewBuilder _ content: () -> Content
+        ) {
+            self.content = content()
+            self.cardWidth = (UIScreen.main.bounds.width - (spacing * 3)) / 2
+            self.cardHeight = cardHeight
+            self._id = _id
+        }
+        
+        var body: some View {
+            content
+                .frame(width: cardWidth, height: _id == UIState.activeCard ? cardHeight : cardHeight - 1, alignment: .center)
+                .scaleEffect(_id == UIState.activeCard ? 1.5 : 0.7)  // Scale effect for active card
+                .padding(.top, _id == UIState.activeCard ? 140 : 0)
+                .animation(.spring)
+        }
+    }
+    
+    // Preview of the ClosetView for development in Xcode
+    struct ClosetView_Previews: PreviewProvider {
+        static var previews: some View {
+            ClosetView(equippedBaby: .constant("Monkey"))
+                .environmentObject(UIStateModel())
         }
     }
 }
 
-// Preview of the ClosetView for development in Xcode
-struct ClosetView_Previews: PreviewProvider {
-    static var previews: some View {
-        ClosetView()  // Display the ClosetView in preview mode
-    }
-}
