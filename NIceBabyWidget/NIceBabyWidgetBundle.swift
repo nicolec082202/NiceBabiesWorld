4bdbb72ddfe78545b40cf1d747e4b5ec2aab0ffc
Carolin <carolinhromero@gmail.com>
2024-11-19 01:33:38 -0500

    functionality bare bones (need to debug)

---
+++ NIceBabyWidget/NIceBabyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NIceBabyWidgetBundle.swift
+//  NIceBabyWidget
+//
+//  Created by carolin heredia on 11/19/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NIceBabyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        NIceBabyWidget()
+    }
+}
