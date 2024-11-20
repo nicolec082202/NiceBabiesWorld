6a6de078e7c559a3ec9777f797cf1d7f01455978
Alejandro Martinez <alejandromart1031@gmail.com>
2024-11-20 18:38:13 -0500

    Updated UI for GameCatalog.

--- NiceBabies/GameCatalogView.swift
+++ NiceBabies/GameCatalogView.swift
@@ -8,56 +8,115 @@ struct GameCatalogView: View {
     @Binding var username: String
 
     var body: some View {
+        
+        //Color.purple
+        
         NavigationView { // Ensure GameCatalogView itself has a NavigationView context
-            GeometryReader { geometry in
-                let width = geometry.size.width
-                let height = geometry.size.height
-
-                ZStack {
-                    HamburgerMenuView(isMenuOpen: false, username: $username, equippedBaby: $equippedBaby, currentView: "GameCatalogView")
+            
+            
+            
+            ZStack{
+                
+                Color(red: 0.8, green: 0.6, blue: 0.9)
+                    .edgesIgnoringSafeArea(.all)
+                
+                GeometryReader { geometry in
+                    let width = geometry.size.width
+                    let height = geometry.size.height
                     
-                    Text("Game Catalog View")
-                        .font(.title)
-                        .position(x: width * 0.5, y: height * 0.1)
-
-                    VStack(spacing: height * 0.02) {
-                        Button(action: {
-                            isWorkoutGamePresented = true
-                        }) {
-                            Text("Launch Workout Game")
-                                .font(.title2)
-                                .padding()
-                                .frame(width: width * 0.8)
-                                .background(Color.green)
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
+                    Image("Game Select Icon")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        //.frame(width: 90)
+                        .padding(.bottom, 350)
+                    
+                    
+                    
+                    
+                    Image(equippedBaby)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        //.frame(width: width * 0.7, height: height * 0.7) // Set frame size relative to view dimensions
+                        .padding(.top, 40)
+                    
+                    
+                    ZStack {
+                        HamburgerMenuView(isMenuOpen: false, username: $username, equippedBaby: $equippedBaby, currentView: "GameCatalogView")
+                        
+                        /*
+                        Text("Game Catalog View")
+                            .font(.title)
+                            .position(x: width * 0.5, y: height * 0.1)
+                         
+                         
+                        
+                        Image("Game Select Icon")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            //.frame(width: 120)
+                        */
+                        
+                        VStack(spacing: height * 0.02) {
+                            
+                            Spacer()
+                            
+                            
+                            HStack{
+                                
+                                
+                                Button(action: {
+                                    isWorkoutGamePresented = true
+                                }) {
+                                    Image("WorkOutGame Button Icon")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 150, height: 150)
+                                    //.padding()
+                                    //.background(Color.green)
+                                        .cornerRadius(10)
+                                }
+                                .fullScreenCover(isPresented: $isWorkoutGamePresented) {
+                                    SpriteKitView(equippedBaby: $equippedBaby, gameType: .workout)
+                                        .edgesIgnoringSafeArea(.all)
+                                }
+                                
+                                
+                                /*
+                                 label: {
+                                 Image("WorkOutGame Button Icon")
+                                 .resizable()
+                                 .aspectRatio(contentMode: .fit)
+                                 .frame(width: 120)
+                                 }
+                                 */
+                                
+                                Button(action: {
+                                    isMatchingGamePresented = true
+                                }) {
+                                    Image("Matching Game Button Icon")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 150, height: 150)
+                                    //.padding()
+                                    //.background(Color.green)
+                                        .cornerRadius(10)
+                                }
+                                .fullScreenCover(isPresented: $isMatchingGamePresented) {
+                                    SpriteKitView(equippedBaby: $equippedBaby, gameType: .matching)
+                                        .edgesIgnoringSafeArea(.all)
+                                }
+                                
+                                
+                            }
+                            
+                            
                         }
-                        .fullScreenCover(isPresented: $isWorkoutGamePresented) {
-                            SpriteKitView(equippedBaby: $equippedBaby, gameType: .workout)
-                                .edgesIgnoringSafeArea(.all)
-                        }
-
-                        Button(action: {
-                            isMatchingGamePresented = true
-                        }) {
-                            Text("Launch Matching Game")
-                                .font(.title2)
-                                .padding()
-                                .frame(width: width * 0.8)
-                                .background(Color.purple)
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                        }
-                        .fullScreenCover(isPresented: $isMatchingGamePresented) {
-                            SpriteKitView(equippedBaby: $equippedBaby, gameType: .matching)
-                                .edgesIgnoringSafeArea(.all)
-                        }
+                        .frame(width: width, height: height)
+                        .position(x: width * 0.5, y: height * 0.5)
                     }
-                    .frame(width: width, height: height)
-                    .position(x: width * 0.5, y: height * 0.5)
                 }
+                .navigationBarHidden(true) // Hide navigation bar if not needed
             }
-            .navigationBarHidden(true) // Hide navigation bar if not needed
         }
     }
 }
