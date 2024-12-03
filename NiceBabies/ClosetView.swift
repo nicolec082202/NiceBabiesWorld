6786e6378a59a57b1ea7ff7643c51bef91915e88
Mandy Saint Simon <mandysaintsimon@gmail.com>
2024-12-02 23:28:54 -0500

    update

--- NiceBabies/ClosetView.swift
+++ NiceBabies/ClosetView.swift
@@ -1,24 +1,21 @@
-
 import SwiftUI
 
 struct CarouselItemModel: Identifiable {
     let id: Int
     let name: String
     let image: Image
-}//end CarouselItemModel
+}
 
 extension Color {
-    // RGB Values
-    static let dropshot = Color(red: 0.976470588, green: 0.5333333, blue: 0.0196078431)
-    static let doublebounce = Color(red: 0.952941176, green: 0.337254902, blue: 0.584313725)
-} //end Color
+    static let dropshot = Color(red: 0.976, green: 0.533, blue: 0.020)
+    static let doublebounce = Color(red: 0.953, green: 0.337, blue: 0.584)
+}
 
 struct ClosetView: View {
     @StateObject var UIState = UIStateModel()
     @Binding var equippedBaby: String
     @Environment(\.presentationMode) var isClosetViewPresented
 
-    
     var body: some View {
         let items = [
             CarouselItemModel(id: 0, name: "Fish", image: Image("NiceBaby_Fish")),
@@ -26,17 +23,16 @@ struct ClosetView: View {
             CarouselItemModel(id: 2, name: "Sheep", image: Image("NiceBaby_Sheep")),
             CarouselItemModel(id: 3, name: "Panda", image: Image("NiceBaby_Panda")),
             CarouselItemModel(id: 4, name: "Cow", image: Image("NiceBaby_Cow")),
-            CarouselItemModel(id: 5, name: "Rabbit", image: Image("NiceBaby_Rabbit")),
-            
+            CarouselItemModel(id: 5, name: "Rabbit", image: Image("NiceBaby_Rabbit"))
         ]
-        
+
         GeometryReader { geometry in
             let screenWidth = geometry.size.width
             let screenHeight = geometry.size.height
             let cardSpacing = screenWidth * 0.08
             let cardWidth = screenWidth * 0.7
             let cardHeight = screenHeight * 0.5
-            
+
             ZStack {
                 Canvas {
                     Carousel(
@@ -55,7 +51,6 @@ struct ClosetView: View {
                                         .resizable()
                                         .aspectRatio(contentMode: .fit)
                                         .frame(width: cardWidth, height: cardHeight)
-                                    
                                 }
                                 .cornerRadius(8)
                                 .transition(.slide)
@@ -65,7 +60,7 @@ struct ClosetView: View {
                     }
                     .environmentObject(UIState)
                 }
-                
+
                 // Close button
                 HStack {
                     Button(action: {
@@ -82,7 +77,7 @@ struct ClosetView: View {
                     Spacer()
                 }
                 .padding(.bottom, screenHeight * 0.9)
-                
+
                 // Page indicator
                 HStack(spacing: 8) {
                     ForEach(0..<items.count, id: \.self) { index in
@@ -93,7 +88,7 @@ struct ClosetView: View {
                     }
                 }
                 .padding(.top, screenHeight * 0.75)
-                
+
                 // Equip button
                 VStack {
                     Button(action: {
@@ -113,144 +108,129 @@ struct ClosetView: View {
             }
         }
     }
-    
-    public class UIStateModel: ObservableObject {
-        @Published var activeCard: Int = 0
-        @Published var screenDrag: Float = 0
-    }//end UIStateModel
-    
-    struct Carousel<Items : View> : View {
-        let items: Items
-        let numberOfItems: CGFloat
-        let spacing: CGFloat
-        let totalSpacing: CGFloat
-        let cardWidth: CGFloat
+}
 
-        @GestureState var isDetectingLongPress = false
-        @EnvironmentObject var UIState: UIStateModel
+public class UIStateModel: ObservableObject {
+    @Published var activeCard: Int = 0
+    @Published var screenDrag: Float = 0
+}
 
-        @inlinable public init(
-            numberOfItems: CGFloat,
-            spacing: CGFloat,
-            cardWidth: CGFloat,
-            @ViewBuilder _ items: () -> Items) {
-            
-            self.items = items()
-            self.numberOfItems = numberOfItems
-            self.spacing = spacing
-            self.cardWidth = cardWidth
-            self.totalSpacing = (numberOfItems - 1) * spacing
-        }
+struct Carousel<Items: View>: View {
+    let items: Items
+    let numberOfItems: CGFloat
+    let spacing: CGFloat
+    let totalSpacing: CGFloat
+    let cardWidth: CGFloat
+    @GestureState var isDetectingLongPress = false
+    @EnvironmentObject var UIState: UIStateModel
 
-        var body: some View {
-            let screenWidth = UIScreen.main.bounds.width
-            let centralizeOffset = (screenWidth - cardWidth) / 2.0
-            let activeOffset = CGFloat(UIState.activeCard) * -(cardWidth + spacing)
-            let totalOffset = centralizeOffset + activeOffset + CGFloat(UIState.screenDrag)
-            
-
-            return HStack(alignment: .top, spacing: spacing) {
-                items
-            }
-            .offset(x: totalOffset, y: 0)
-            .animation(.spring(response: 0.5, dampingFraction: 0.8), value: UIState.activeCard)
-            .gesture(
-                DragGesture()
-                    .updating($isDetectingLongPress) { currentState, gestureState, transaction in
-                        UIState.screenDrag = Float(currentState.translation.width) + 145
-                    }
-                    .onEnded { value in
-                        UIState.screenDrag = 145
-                        if (value.translation.width < -50) && UIState.activeCard < Int(numberOfItems) - 1 {
-                            withAnimation(.spring()) {
-                                UIState.activeCard += 1
-                            }
-                            let impactMed = UIImpactFeedbackGenerator(style: .medium)
-                            impactMed.impactOccurred()
-                        }
-                        if (value.translation.width > 50) && UIState.activeCard > 0 {
-                            withAnimation(.spring()) {
-                                UIState.activeCard -= 1
-                            }
-                            let impactMed = UIImpactFeedbackGenerator(style: .medium)
-                            impactMed.impactOccurred()
-                        }
-                    }
-            )
-        }
+    @inlinable public init(
+        numberOfItems: CGFloat,
+        spacing: CGFloat,
+        cardWidth: CGFloat,
+        @ViewBuilder _ items: () -> Items
+    ) {
+        self.items = items()
+        self.numberOfItems = numberOfItems
+        self.spacing = spacing
+        self.cardWidth = cardWidth
+        self.totalSpacing = (numberOfItems - 1) * spacing
     }
-    
-    
-    
-    struct Canvas<Content : View> : View {
-        let content: Content
-        @EnvironmentObject var UIState: UIStateModel
-        
-        @inlinable init(@ViewBuilder _ content: () -> Content) {
-            self.content = content()
-            
+
+    var body: some View {
+        let screenWidth = UIScreen.main.bounds.width
+        let centralizeOffset = (screenWidth - cardWidth) / 2.0
+        let activeOffset = CGFloat(UIState.activeCard) * -(cardWidth + spacing)
+        let totalOffset = centralizeOffset + activeOffset + CGFloat(UIState.screenDrag)
+
+        return HStack(alignment: .top, spacing: spacing) {
+            items
         }
-        
-        //Background image
-        var body: some View {
-            
-            GeometryReader { geometry in
-                let width = geometry.size.width
-                let height = geometry.size.height
-                content
-                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-                    .background(Image("HomePage Background")
+        .offset(x: totalOffset, y: 0)
+        .animation(.spring(response: 0.5, dampingFraction: 0.8), value: UIState.activeCard)
+        .gesture(
+            DragGesture()
+                .updating($isDetectingLongPress) { currentState, gestureState, transaction in
+                    UIState.screenDrag = Float(currentState.translation.width)
+                }
+                .onEnded { value in
+                    UIState.screenDrag = 0
+                    if (value.translation.width < -50) && UIState.activeCard < Int(numberOfItems) - 1 {
+                        withAnimation(.spring()) {
+                            UIState.activeCard += 1
+                        }
+                        let impactMed = UIImpactFeedbackGenerator(style: .medium)
+                        impactMed.impactOccurred()
+                    }
+                    if (value.translation.width > 50) && UIState.activeCard > 0 {
+                        withAnimation(.spring()) {
+                            UIState.activeCard -= 1
+                        }
+                        let impactMed = UIImpactFeedbackGenerator(style: .medium)
+                        impactMed.impactOccurred()
+                    }
+                }
+        )
+    }
+}
+
+struct Canvas<Content: View>: View {
+    let content: Content
+    @EnvironmentObject var UIState: UIStateModel
+
+    @inlinable init(@ViewBuilder _ content: () -> Content) {
+        self.content = content()
+    }
+
+    var body: some View {
+        GeometryReader { geometry in
+            let width = geometry.size.width
+            let height = geometry.size.height
+            content
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                .background(
+                    Image("HomePage Background")
                         .resizable()
                         .ignoresSafeArea()
-                        .frame(width: width*1.59, height: height)
+                        .frame(width: width * 1.59, height: height)
                         .position(x: width * 0.5, y: height * 0.5)
-                        .blur(radius: 5, opaque: true))
-            }
-        }//end body
-        
-    }//end canvas
-    
-    struct Item<Content: View>: View {
-        
-        @EnvironmentObject var UIState: UIStateModel
-        let cardWidth: CGFloat
-        let cardHeight: CGFloat
-        var _id: Int
-        var content: Content
-        
-        @inlinable public init(
-            _id: Int,
-            spacing: CGFloat,
-            //widthOfHiddenCards: CGFloat,
-            cardHeight: CGFloat,
-            @ViewBuilder _ content: () -> Content
-        ) {
-            self.content = content()
-            self.cardWidth = (UIScreen.main.bounds.width - (spacing * 3)) / 2
-            self.cardHeight = cardHeight
-            self._id = _id
-        }// end inlinable
-        
-        var body: some View {
-            content
-                .frame(width: cardWidth, height: _id == UIState.activeCard ? cardHeight : cardHeight - 1, alignment: .center)
-                .scaleEffect(_id == UIState.activeCard ? 1.5 : 0.7)  // Scale effect for active card
-                .padding(.top, _id == UIState.activeCard ? 140 : 0)
-                .animation(.spring())
-        }// end body
-        
-    }//endItem
-    
-    
-    
-    // Preview of the ClosetView for development in Xcode
-    struct ClosetView_Previews: PreviewProvider {
-        static var previews: some View {
-            ClosetView(equippedBaby: .constant("Monkey"))
-                .environmentObject(UIStateModel())
+                        .blur(radius: 5, opaque: true)
+                )
         }
-    }//end ClosetView_Previews
+    }
+}
 
-    
-}//end ClosetView
+struct Item<Content: View>: View {
+    @EnvironmentObject var UIState: UIStateModel
+    let cardWidth: CGFloat
+    let cardHeight: CGFloat
+    var _id: Int
+    var content: Content
 
+    @inlinable public init(
+        _id: Int,
+        spacing: CGFloat,
+        cardHeight: CGFloat,
+        @ViewBuilder _ content: () -> Content
+    ) {
+        self.content = content()
+        self.cardWidth = (UIScreen.main.bounds.width - (spacing * 3)) / 2
+        self.cardHeight = cardHeight
+        self._id = _id
+    }
+
+    var body: some View {
+        content
+            .frame(width: cardWidth, height: _id == UIState.activeCard ? cardHeight : cardHeight - 1, alignment: .center)
+            .scaleEffect(_id == UIState.activeCard ? 1.2 : 0.8) // Adjusted scale effect for active card
+            .padding(.top, _id == UIState.activeCard ? 140 : 0)
+            .animation(.spring())
+    }
+}
+
+struct ClosetView_Previews: PreviewProvider {
+    static var previews: some View {
+        ClosetView(equippedBaby: .constant("Monkey"))
+            .environmentObject(UIStateModel())
+    }
+}
